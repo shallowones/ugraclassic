d583da099d1cc71bd8df5ff082bd21a653a342b6
Павел Белинович < >
2016-11-26 16:24:16 +0500

    Сверстана страница Наша команда ticket#131061/4162536/4162771

---
+++ local/html/team.php
@@ -0,0 +1,56 @@
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Наша команда");
+?>
+
+<ul class="team">
+    <li class="team__item">
+        <div class="team-img">
+            <img src="<? echo SITE_TEMPLATE_PATH . '/img/no-photo_w.png' ?>">
+        </div>
+        <div class="team-name">Журавлева Лариса Николаевна</div>
+        <div class="team-phone">+7 908 <span>912-31-84</span></div>
+        <p>Информация о мероприятиях</p>
+    </li>
+    <li class="team__item">
+        <div class="team-img">
+            <img src="<? echo SITE_TEMPLATE_PATH . '/img/no-photo_m.png' ?>">
+        </div>
+        <div class="team-name">Койнова Татьяна Александровна</div>
+        <div class="team-phone">+7 908 <span>912-31-84</span></div>
+        <p>Расскажет обо всех мероприятиях культуры Югры, поделится самыми свежими новостями культуры и тп.</p>
+    </li>
+    <li class="team__item">
+        <div class="team-img">
+            <img src="<? echo SITE_TEMPLATE_PATH . '/img/content/23.png' ?>">
+        </div>
+        <div class="team-name">Мальцева Яна Анатольевна</div>
+        <div class="team-phone">+7 908 <span>912-31-84</span></div>
+        <p>Информация о мероприятиях</p>
+    </li>
+    <li class="team__item">
+        <div class="team-img">
+            <img src="<? echo SITE_TEMPLATE_PATH . '/img/content/24.png' ?>">
+        </div>
+        <div class="team-name">Скакун Елена Александровна</div>
+        <div class="team-phone">+7 908 <span>912-31-84</span></div>
+        <p>Расскажет обо всех мероприятиях культуры Югры, поделится самыми свежими новостями культуры и тп.</p>
+    </li>
+    <li class="team__item">
+        <div class="team-img">
+            <img src="<? echo SITE_TEMPLATE_PATH . '/img/content/25.png' ?>">
+        </div>
+        <div class="team-name">Буряк Ольга Анатольевна</div>
+        <div class="team-phone">+7 908 <span>912-31-84</span></div>
+        <p>Расскажет обо всех мероприятиях культуры Югры, поделится самыми свежими новостями культуры и тп.</p>
+    </li>
+    <li class="team__item">
+        <div class="team-img">
+            <img src="<? echo SITE_TEMPLATE_PATH . '/img/content/26.png' ?>">
+        </div>
+        <div class="team-name">Осокин Николай Николаевич</div>
+        <div class="team-phone">+7 908 <span>912-31-84</span></div>
+        <p>Расскажет обо всех мероприятиях культуры Югры, поделится самыми свежими новостями культуры и тп.</p>
+    </li>
+</ul>
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
