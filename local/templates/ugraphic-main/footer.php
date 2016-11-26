d0a992c9a49896a79583ac2c5b20e6d85982a67f
Vladimir <spasennikov-vs@yandex.ru>
2016-11-26 18:04:59 +0500

    ticket#131061/3615860/4162280

--- local/templates/ugraphic-main/footer.php
+++ local/templates/ugraphic-main/footer.php
@@ -59,6 +59,20 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 
 		</div><!-- .wrapper -->
 	</div><!-- #footer -->
-
+    <!-- Rating@Mail.ru counter -->
+    <script type="text/javascript">
+        var _tmr = window._tmr || (window._tmr = []);
+        _tmr.push({id: "2837175", type: "pageView", start: (new Date()).getTime()});
+        (function (d, w, id) {
+            if (d.getElementById(id)) return;
+            var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+            ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+            var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+            if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+        })(document, window, "topmailru-code");
+    </script><noscript><div style="position:absolute;left:-10000px;">
+            <img src="//top-fwz1.mail.ru/counter?id=2837175;js=na" style="border:0;" height="1" width="1" alt="Рейтинг@Mail.ru" />
+        </div></noscript>
+    <!-- //Rating@Mail.ru counter -->
 	</body>
 </html>
