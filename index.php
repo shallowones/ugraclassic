96a013984b54dfbe3ddeaceff00d16badaaca365
Nastia <vau@ugraweb.ru>
2016-12-06 14:33:08 +0500

    Информационные партнеры ticket#131061/3615860/4204863

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use Bitrix\Main\Page\Asset;
 Asset::getInstance()->addCss("/index.css");
 
 $ibEventsID 		= 	\UW\IBBase::getIBIdByCode("events");
+$ibInformPartnersID	= 	\UW\IBBase::getIBIdByCode("inform_partners");
 $ibNewsID 			= 	\UW\IBBase::getIBIdByCode("news");
 $ibNewsCultureID 	= 	\UW\IBBase::getIBIdByCode("news_culture");
 $ibGroupsID			=	\UW\IBBase::getIBIdByCode("groups");
@@ -779,9 +780,74 @@ $ibPartnersID		=	\UW\IBBase::getIBIdByCode("partners");
 	),
 	false
 );?>
+            <h1>ИНФОРМАЦИОННЫЕ ПАРТНЕРЫ</h1>
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:news.list",
+                "inform-partners-carousel",
+                array(
+                    "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                    "ADD_SECTIONS_CHAIN" => "N",
+                    "AJAX_MODE" => "N",
+                    "AJAX_OPTION_ADDITIONAL" => "",
+                    "AJAX_OPTION_HISTORY" => "N",
+                    "AJAX_OPTION_JUMP" => "N",
+                    "AJAX_OPTION_STYLE" => "Y",
+                    "CACHE_FILTER" => "N",
+                    "CACHE_GROUPS" => "Y",
+                    "CACHE_TIME" => "36000000",
+                    "CACHE_TYPE" => "A",
+                    "CHECK_DATES" => "Y",
+                    "DETAIL_URL" => "",
+                    "DISPLAY_BOTTOM_PAGER" => "N",
+                    "DISPLAY_DATE" => "N",
+                    "DISPLAY_NAME" => "Y",
+                    "DISPLAY_PICTURE" => "Y",
+                    "DISPLAY_PREVIEW_TEXT" => "N",
+                    "DISPLAY_TOP_PAGER" => "N",
+                    "FIELD_CODE" => array(
+                        0 => "",
+                        1 => "",
+                    ),
+                    "FILTER_NAME" => "",
+                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                    "IBLOCK_ID" => $ibInformPartnersID,
+                    "IBLOCK_TYPE" => "groups",
+                    "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                    "INCLUDE_SUBSECTIONS" => "Y",
+                    "MESSAGE_404" => "",
+                    "NEWS_COUNT" => "20",
+                    "PAGER_BASE_LINK_ENABLE" => "N",
+                    "PAGER_DESC_NUMBERING" => "N",
+                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                    "PAGER_SHOW_ALL" => "N",
+                    "PAGER_SHOW_ALWAYS" => "N",
+                    "PAGER_TEMPLATE" => ".default",
+                    "PAGER_TITLE" => "Новости",
+                    "PARENT_SECTION" => "",
+                    "PARENT_SECTION_CODE" => "",
+                    "PREVIEW_TRUNCATE_LEN" => "",
+                    "PROPERTY_CODE" => array(
+                        0 => "URL",
+                        1 => "",
+                    ),
+                    "SET_BROWSER_TITLE" => "N",
+                    "SET_LAST_MODIFIED" => "N",
+                    "SET_META_DESCRIPTION" => "Y",
+                    "SET_META_KEYWORDS" => "Y",
+                    "SET_STATUS_404" => "N",
+                    "SET_TITLE" => "N",
+                    "SHOW_404" => "N",
+                    "SORT_BY1" => "ID",
+                    "SORT_BY2" => "SORT",
+                    "SORT_ORDER1" => "ASC",
+                    "SORT_ORDER2" => "ASC",
+                    "COMPONENT_TEMPLATE" => "partners-list"
+                ),
+                false
+            );?>
 
 		</div><!-- .wrapper -->
-	</div><!-- 3d tour -->
+	</div>
 
     <div class="partners-box"> <!-- partners -->
         <div class="wrapper">
---
+++ local/templates/ugraphic-main/components/bitrix/news.list/inform-partners-carousel/template.php
@@ -0,0 +1,62 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="panaram3d-box">
+    <div id="panaram3d-partner2" class="owl-carousel owl-theme">
+    <?foreach($arResult["ITEMS"] as $arItem):?>
+        <? $slide = CFile::ResizeImageGet($arItem["PREVIEW_PICTURE"], array('width'=>400, 'height'=>285), BX_RESIZE_IMAGE_EXACT, true);?>
+        <?
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+        <a href="<?=$arItem["DISPLAY_PROPERTIES"]["URL"]["~VALUE"]?>" alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>" title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>">
+            <div class="item-3d" id="<?=$this->GetEditAreaId($arItem['ID']);?>" style="background:url('<?=$slide["src"]?>') no-repeat center center;">
+            </div>
+        </a>
+    <?endforeach;?>
+    </div>
+</div>
+<script>
+    var owl = $('#panaram3d-partner2');
+    owl.owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots : true,
+        autoplay : false,
+        autoplayTimeout : 15000,
+        autoplayHoverPause : true,
+        smartSpeed : 500,
+        navText : ["<i></i>","<i></i>"],
+        responsive:{
+            0:{
+                items:2,
+                mouseDrag : true,
+            },
+            758:{
+                items:3,
+                mouseDrag : true,
+            },
+            1000:{
+                items:4,
+                mouseDrag : false,
+            },
+            1400:{
+                items:5,
+                mouseDrag : false,
+            }
+        }
+    });
+
+</script>
