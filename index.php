35e55867283090573b30b1d4ecd8deb0b9b6c0f7
Nastia <vau@ugraweb.ru>
2016-12-06 11:38:53 +0500

    Скрыт блок Мероприятия по региону на главной ticket#131061/3615860/4204834

--- index.php
+++ index.php
@@ -540,79 +540,79 @@ $ibPartnersID		=	\UW\IBBase::getIBIdByCode("partners");
 	</div><!-- calendar line -->
 */?>
 
-	<div class="events-box"> <!-- calendar line -->
-		<div class="wrapper">
-
-		<h1>МЕРОПРИЯТИЯ ПО РЕГИОНУ</h1>
-
-		<?$APPLICATION->IncludeComponent("bitrix:news.list", "slider-events-region", array(
-			"ACTIVE_DATE_FORMAT" => "j F Y",
-				"ADD_SECTIONS_CHAIN" => "N",
-				"AJAX_MODE" => "N",
-				"AJAX_OPTION_ADDITIONAL" => "",
-				"AJAX_OPTION_HISTORY" => "N",
-				"AJAX_OPTION_JUMP" => "N",
-				"AJAX_OPTION_STYLE" => "Y",
-				"CACHE_FILTER" => "Y",
-				"CACHE_GROUPS" => "N",
-				"CACHE_TIME" => "36000000",
-				"CACHE_TYPE" => "N",
-				"CHECK_DATES" => "Y",
-				"DETAIL_URL" => "",
-				"DISPLAY_BOTTOM_PAGER" => "N",
-				"DISPLAY_DATE" => "N",
-				"DISPLAY_NAME" => "Y",
-				"DISPLAY_PICTURE" => "Y",
-				"DISPLAY_PREVIEW_TEXT" => "N",
-				"DISPLAY_TOP_PAGER" => "N",
-				"FIELD_CODE" => array(
-					0 => "",
-					1 => "",
-				),
-				"FILTER_NAME" => "",
-				"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-				"IBLOCK_ID" => $ibRegionEventsID,
-				"IBLOCK_TYPE" => "afisha",
-				"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-				"INCLUDE_SUBSECTIONS" => "Y",
-				"MESSAGE_404" => "",
-				"NEWS_COUNT" => "20",
-				"PAGER_BASE_LINK_ENABLE" => "N",
-				"PAGER_DESC_NUMBERING" => "N",
-				"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-				"PAGER_SHOW_ALL" => "N",
-				"PAGER_SHOW_ALWAYS" => "N",
-				"PAGER_TEMPLATE" => ".default",
-				"PAGER_TITLE" => "Новости",
-				"PARENT_SECTION" => "",
-				"PARENT_SECTION_CODE" => "",
-				"PREVIEW_TRUNCATE_LEN" => "",
-				"PROPERTY_CODE" => array(
-					0 => "age",
-					1 => "date",
-					2 => "hall",
-					3 => "",
-				),
-				"SET_BROWSER_TITLE" => "N",
-				"SET_LAST_MODIFIED" => "N",
-				"SET_META_DESCRIPTION" => "Y",
-				"SET_META_KEYWORDS" => "N",
-				"SET_STATUS_404" => "N",
-				"SET_TITLE" => "N",
-				"SHOW_404" => "N",
-				"SORT_BY1" => "ACTIVE_FROM",
-				"SORT_BY2" => "SORT",
-				"SORT_ORDER1" => "DESC",
-				"SORT_ORDER2" => "ASC",
-				"COMPONENT_TEMPLATE" => "slider-events-region"
-			),
-			false,
-			array(
-			"ACTIVE_COMPONENT" => "Y"
-			)
-		);?>
-		</div><!-- .wrapper -->
-	</div><!-- .calendar line -->
+<!--	<div class="events-box"> <!-- calendar line -->-->
+<!--		<div class="wrapper">-->
+<!---->
+<!--		<h1>МЕРОПРИЯТИЯ ПО РЕГИОНУ</h1>-->
+<!---->
+<!--		--><?//$APPLICATION->IncludeComponent("bitrix:news.list", "slider-events-region", array(
+//			"ACTIVE_DATE_FORMAT" => "j F Y",
+//				"ADD_SECTIONS_CHAIN" => "N",
+//				"AJAX_MODE" => "N",
+//				"AJAX_OPTION_ADDITIONAL" => "",
+//				"AJAX_OPTION_HISTORY" => "N",
+//				"AJAX_OPTION_JUMP" => "N",
+//				"AJAX_OPTION_STYLE" => "Y",
+//				"CACHE_FILTER" => "Y",
+//				"CACHE_GROUPS" => "N",
+//				"CACHE_TIME" => "36000000",
+//				"CACHE_TYPE" => "N",
+//				"CHECK_DATES" => "Y",
+//				"DETAIL_URL" => "",
+//				"DISPLAY_BOTTOM_PAGER" => "N",
+//				"DISPLAY_DATE" => "N",
+//				"DISPLAY_NAME" => "Y",
+//				"DISPLAY_PICTURE" => "Y",
+//				"DISPLAY_PREVIEW_TEXT" => "N",
+//				"DISPLAY_TOP_PAGER" => "N",
+//				"FIELD_CODE" => array(
+//					0 => "",
+//					1 => "",
+//				),
+//				"FILTER_NAME" => "",
+//				"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+//				"IBLOCK_ID" => $ibRegionEventsID,
+//				"IBLOCK_TYPE" => "afisha",
+//				"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+//				"INCLUDE_SUBSECTIONS" => "Y",
+//				"MESSAGE_404" => "",
+//				"NEWS_COUNT" => "20",
+//				"PAGER_BASE_LINK_ENABLE" => "N",
+//				"PAGER_DESC_NUMBERING" => "N",
+//				"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+//				"PAGER_SHOW_ALL" => "N",
+//				"PAGER_SHOW_ALWAYS" => "N",
+//				"PAGER_TEMPLATE" => ".default",
+//				"PAGER_TITLE" => "Новости",
+//				"PARENT_SECTION" => "",
+//				"PARENT_SECTION_CODE" => "",
+//				"PREVIEW_TRUNCATE_LEN" => "",
+//				"PROPERTY_CODE" => array(
+//					0 => "age",
+//					1 => "date",
+//					2 => "hall",
+//					3 => "",
+//				),
+//				"SET_BROWSER_TITLE" => "N",
+//				"SET_LAST_MODIFIED" => "N",
+//				"SET_META_DESCRIPTION" => "Y",
+//				"SET_META_KEYWORDS" => "N",
+//				"SET_STATUS_404" => "N",
+//				"SET_TITLE" => "N",
+//				"SHOW_404" => "N",
+//				"SORT_BY1" => "ACTIVE_FROM",
+//				"SORT_BY2" => "SORT",
+//				"SORT_ORDER1" => "DESC",
+//				"SORT_ORDER2" => "ASC",
+//				"COMPONENT_TEMPLATE" => "slider-events-region"
+//			),
+//			false,
+//			array(
+//			"ACTIVE_COMPONENT" => "Y"
+//			)
+//		);?>
+<!--		</div><!-- .wrapper -->-->
+<!--	</div><!-- .calendar line -->-->
 
 
 	<div class="uk-today-box"> <!-- UK today -->
