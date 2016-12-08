8b1c73689f0f579c2c7fe2d4b7d606fc9d66ce5c
Vladimir <spasennikov-vs@yandex.ru>
2016-12-08 19:07:53 +0500

    ticket#131061/4189453/4216392

--- local/templates/visit/components/ugraweb/collective/collective/nf_404.php
+++ local/templates/visit/components/ugraweb/collective/collective/nf_404.php
@@ -4,3 +4,4 @@
 
 <p><a href="kontsertnyy-orkestr-yugry/">Концертный оркестр Югры</a></p>
 <p><a href="dukh-or-yugry/">Симфонический оркестр</a></p>
+<p><a href="mlada/">Ансамбль русской песни «Млада»</a></p>
