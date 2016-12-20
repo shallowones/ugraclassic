a59e033058b6b538f3c6cabd4ac9cca03ce1f3c6
BPA <bpa@ugraweb.ru>
2016-12-20 14:09:21 +0500

    Правки шапки сайта [2]

--- local/templates/ugraphic-main/components/bitrix/menu/top-menu-multilevel/template.php
+++ local/templates/ugraphic-main/components/bitrix/menu/top-menu-multilevel/template.php
@@ -1,70 +1,80 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
-<?if (!empty($arResult)):?>
+<? if (!empty($arResult)): ?>
 
-<nav class="clearfix">
-	<a href="#" id="pull">
-        <div class="nav-icon"></div>
-    </a>
+    <nav class="clearfix">
+        <a href="#" id="pull">
+            <div class="nav-icon"></div>
+        </a>
 
 
-    <div class="mobile-menu">
+        <div class="mobile-menu">
 
-        <div class="conts-hidden"></div>
-<ul class="top-menu" id="top-menu-1">
-<?
-$previousLevel = 0;
-foreach($arResult as $arItem):?>
+            <ul class="top-menu" id="top-menu-1">
+                <?
+                $previousLevel = 0;
+                foreach ($arResult as $arItem): ?>
 
-	<?if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
-		<?=str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"]));?>
-	<?endif?>
+                <? if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel): ?>
+                    <?= str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"])); ?>
+                <? endif ?>
 
-	<?if ($arItem["IS_PARENT"]):?>
+                <? if ($arItem["IS_PARENT"]): ?>
 
-		<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-			<li><a href="<?=$arItem["LINK"]?>" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a>
-				<ul class="falls">
-		<?else:?>
-			<li <?if ($arItem["SELECTED"]):?>class="item-selected"<?endif?>><a href="<?=$arItem["LINK"]?>" class="parent"><?=$arItem["TEXT"]?></a>
-				<ul>
-		<?endif?>
+                <? if ($arItem["DEPTH_LEVEL"] == 1): ?>
+                <li><a href="<?= $arItem["LINK"] ?>"
+                       class="<? if ($arItem["SELECTED"]): ?>root-item-selected<? else: ?>root-item<? endif ?>"><?= $arItem["TEXT"] ?></a>
+                    <ul class="falls">
+                        <? else: ?>
+                        <li <? if ($arItem["SELECTED"]): ?>class="item-selected"<? endif ?>><a
+                                    href="<?= $arItem["LINK"] ?>" class="parent"><?= $arItem["TEXT"] ?></a>
+                            <ul>
+                                <? endif ?>
 
-	<?else:?>
+                                <? else: ?>
 
-		<?if ($arItem["PERMISSION"] > "D"):?>
+                                    <? if ($arItem["PERMISSION"] > "D"): ?>
 
-			<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-				<li><a href="<?=$arItem["LINK"]?>" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>"><?=$arItem["TEXT"]?></a></li>
-			<?else:?>
-				<li<?if ($arItem["SELECTED"]):?> class="item-selected"<?endif?>><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
-			<?endif?>
+                                        <? if ($arItem["DEPTH_LEVEL"] == 1): ?>
+                                            <li><a href="<?= $arItem["LINK"] ?>"
+                                                   class="<? if ($arItem["SELECTED"]): ?>root-item-selected<? else: ?>root-item<? endif ?>"><?= $arItem["TEXT"] ?></a>
+                                            </li>
+                                        <? else: ?>
+                                            <li<? if ($arItem["SELECTED"]): ?> class="item-selected"<? endif ?>><a
+                                                        href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
+                                        <? endif ?>
 
-		<?else:?>
+                                    <? else: ?>
 
-			<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-				<li><a href="" class="<?if ($arItem["SELECTED"]):?>root-item-selected<?else:?>root-item<?endif?>" title="<?=GetMessage("MENU_ITEM_ACCESS_DENIED")?>"><?=$arItem["TEXT"]?></a></li>
-			<?else:?>
-				<li><a href="" class="denied" title="<?=GetMessage("MENU_ITEM_ACCESS_DENIED")?>"><?=$arItem["TEXT"]?></a></li>
-			<?endif?>
+                                        <? if ($arItem["DEPTH_LEVEL"] == 1): ?>
+                                            <li><a href=""
+                                                   class="<? if ($arItem["SELECTED"]): ?>root-item-selected<? else: ?>root-item<? endif ?>"
+                                                   title="<?= GetMessage("MENU_ITEM_ACCESS_DENIED") ?>"><?= $arItem["TEXT"] ?></a>
+                                            </li>
+                                        <? else: ?>
+                                            <li><a href="" class="denied"
+                                                   title="<?= GetMessage("MENU_ITEM_ACCESS_DENIED") ?>"><?= $arItem["TEXT"] ?></a>
+                                            </li>
+                                        <? endif ?>
 
-		<?endif?>
+                                    <? endif ?>
 
-	<?endif?>
+                                <? endif ?>
 
-	<?$previousLevel = $arItem["DEPTH_LEVEL"];?>
+                                <? $previousLevel = $arItem["DEPTH_LEVEL"]; ?>
 
-<?endforeach?>
+                                <? endforeach ?>
 
-<?if ($previousLevel > 1)://close last item tags?>
-	<?=str_repeat("</ul></li>", ($previousLevel-1) );?>
-<?endif?>
+                                <? if ($previousLevel > 1)://close last item tags?>
+                                    <?= str_repeat("</ul></li>", ($previousLevel - 1)); ?>
+                                <? endif ?>
 
-</ul>
-<?/*<div class="menu-clear-left"></div>
+                            </ul>
+                            <? /*<div class="menu-clear-left"></div>
                 <ul class="t-menu"></ul>
-                <div class="menu-clear-left"></div>*/?>
-    </div>
-</nav>
+                <div class="menu-clear-left"></div>*/ ?>
+                            <div class="conts-hidden"></div>
+        </div>
+    </nav>
 
-<?endif?>
+<? endif ?>
