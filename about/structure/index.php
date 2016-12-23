eab1d26006f2f8fe7b45116aa47788c5830df024
BPA <bpa@ugraweb.ru>
2016-12-23 15:23:47 +0500

    Правки контентной зоны раздела О КТЦ

--- about/structure/index.php
+++ about/structure/index.php
@@ -1,4 +1,4 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Структура");
-?><a href="/upload/medialibrary/7b7/7b7de3dafcabdcea73822556336b81ec.png" target="_blank"><img width="600" alt="structure img.png" src="/upload/medialibrary/7b7/7b7de3dafcabdcea73822556336b81ec.png" height="486" title="structure img.png"></a><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><a href="/upload/medialibrary/7b7/7b7de3dafcabdcea73822556336b81ec.png" target="_blank"><img alt="structure img.png" src="/upload/medialibrary/7b7/7b7de3dafcabdcea73822556336b81ec.png" title="structure img.png" style="width: 100%"></a><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
